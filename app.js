ca27a6b8c888c06067eeb704be7cd3e269aa3708
Ritu kukreja <104751465+Ritu0208@users.noreply.github.com>
2022-08-31 21:10:50 +0530

    updated files

---
+++ app.js
@@ -0,0 +1,30 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// var serverUrl = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
+var serverUrl = 'https://api.funtranslations.com/translate/dolan.json';
+
+function getTranslationUrl(input){
+    return serverUrl + "?" + "text=" + input;
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Something wrong with server try again after sometime")
+}
+function clickEventHandler(){
+
+    var inputText = txtInput.value; //taking input
+
+    //calling server for processing
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated
+            outputDiv.innerText = translatedText //output
+        
+        })
+        .catch(errorHandler);
+ }
+btnTranslate.addEventListener("click",  clickEventHandler);
